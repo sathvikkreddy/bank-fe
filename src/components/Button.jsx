0ab2ef8878612f3ee7eb24229a1b83037157f910
sathvikkreddy <sathvikreddy8685@gmail.com>
2024-05-12 20:21:34 +0530

    added className to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Button = (props) => {
   return (
-    <div
-      className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md bg-black cursor-pointer shadow-sm text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black`}
-    >
+    <div className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md bg-black cursor-pointer shadow-sm text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black ${props.className}`}>
       <button onClick={props.onClick}>{props.title}</button>
     </div>
   );

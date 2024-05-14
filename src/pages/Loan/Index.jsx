a1b7991b9561e818c1a6236b0d23e77a191e7f93
npankajkumar <nandadeakash5704@gmail.com>
2024-05-14 19:22:50 +0530

    d

--- src/pages/Loan/Index.jsx
+++ src/pages/Loan/Index.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import NotFound from "../NotFound";
-
+import Goldloan from "./Goldloan";
 const Loan = () => {
   return (
     <Routes>
       <Route path="/" element={<div>Loan</div>} />
+      <Route path="/goldloan" element={<Goldloan/>} />
+      <Route path="/personalloan" element={<div>personalLoan</div>} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
   // create your own pages by adding new <Route/> by default the path has /yourmodule, just specify page name for path
   // Ex: for life insurance page in insurance add <Route path="/life" element={your component} />
   // do not include full url in path like <Route path="/insurance/life" element={your component} />;
+
 };
 
 export default Loan;

8cd991d974db258fbe28713a2cc1701225abfeeb
sathvikkreddy <sathvikreddy8685@gmail.com>
2024-05-17 10:50:11 +0530

    modified card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,8 @@ import React from "react";
 const Card = (props) => {
   return (
     <div className="h-full border rounded-md w-full flex-cols shadow-xl">
-      <div className="h-1/6 border-b shadow-md shadow-slate-100 text-xl font-semibold flex flex-col justify-center p-2">
-        {props.title}
-      </div>
-      <div className="p-2 h-5/6">{props.children}</div>
+      <div className="h-1/6 min-h-12 border-b shadow-md shadow-slate-100 text-xl font-semibold flex flex-col justify-center p-2">{props.title}</div>
+      <div className="pb-3 pt-2 px-2 h-5/6">{props.children}</div>
     </div>
   );
 };

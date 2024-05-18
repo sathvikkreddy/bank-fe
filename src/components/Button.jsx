c79f399f89fd8db0cfa29bfb6efb0d853e36b86d
sathvikkreddy <sathvikreddy8685@gmail.com>
2024-05-18 16:57:53 +0530

    fixed button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Button = (props) => {
   return (
-    <div className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md bg-black cursor-pointer shadow-sm text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black ${props.className}`}>
-      <button onClick={props.onClick}>{props.title}</button>
+    <div onClick={props.onClick} className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md bg-black cursor-pointer shadow-sm text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black ${props.className}`}>
+      {props.title}
     </div>
   );
 };

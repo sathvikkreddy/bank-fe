cfcf114d724b1ec5c41e22e9a72de774166bfca3
sathvikkreddy <sathvikreddy8685@gmail.com>
2024-05-21 19:54:51 +0530

    fixed wrapping issue

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ onClick, loading, title, loadingTitle, className }) => {
   const isLoading = loading || false;
   return (
-    <div onClick={onClick} className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md bg-black cursor-${isLoading ? "not-allowed" : "pointer"} shadow-sm text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black ${className}`}>
+    <div onClick={onClick} className={`w-full flex justify-center text-nowrap py-2 px-4 border border-transparent rounded-md bg-black cursor-${isLoading ? "not-allowed" : "pointer"} shadow-sm text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black ${className}`}>
       {isLoading ? loadingTitle : title}
     </div>
   );

748bb26101acb045b70505856f0ecfb5a5f0a486
sathvikkreddy <sathvikreddy8685@gmail.com>
2024-05-27 20:59:17 +0530

    added react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Toaster position="top-right" reverseOrder={true} />
     <BrowserRouter>
       <App />
     </BrowserRouter>

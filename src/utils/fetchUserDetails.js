ec1967538caf81fb646d241e7a08c84f112148a6
sathvikkreddy <sathvikreddy8685@gmail.com>
2024-06-19 16:52:49 +0530

    handled invalid token

--- src/utils/fetchUserDetails.js
+++ src/utils/fetchUserDetails.js
@@ -1,15 +1,25 @@
 import axios from "axios";
+import toast from "react-hot-toast";
+import { redirect } from "react-router-dom";
 
 export const fetchUserDetails = async () => {
   try {
     const token = localStorage.getItem("authorization");
     if (!token) throw new Error("Unauthorized: Token not found");
-    const userResponse = await axios.get("https://techbuzzers.somee.com/GetUserDetails", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const userResponse = await axios.get(
+      "https://techbuzzers.somee.com/GetUserDetails",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (userResponse.status === 401) {
+      toast.error("Token expired. Login again!");
+      redirect("/signin");
+      return;
+    }
     return userResponse.data;
   } catch (err) {
     return err.message;
